9793295e420cf78813a829c51eb8a547849ed271
Khoa Nguyen <ktg@qlik.com>
2016-09-15 14:45:59 +0200

    update test script

--- scripts/run_large_test.sh
+++ scripts/run_large_test.sh
@@ -10,11 +10,68 @@ cd $(dirname $0)/../../grpc_benchmark-build
 
 sleep 2
 
-#./datastreamclient --num_clients 1 --data_size 10 --num_iterations 10 --chunk_size 64
 
-./datastreamclient --num_clients 1 --data_size 500 --num_iterations 10 --chunk_size 1024
+####### 1 client
+# ./datastreamclient --num_clients 1 --data_size 10 --num_iterations 10 --chunk_size 64
+# ./datastreamclient --num_clients 1 --data_size 10 --num_iterations 10 --chunk_size 128
+# ./datastreamclient --num_clients 1 --data_size 10 --num_iterations 10 --chunk_size 256
+# ./datastreamclient --num_clients 1 --data_size 10 --num_iterations 10 --chunk_size 512
+# ./datastreamclient --num_clients 1 --data_size 10 --num_iterations 10 --chunk_size 1024
 
-# 2 clients
-#./datastreamclient --num_clients 2 --data_size 10 --num_iterations 10 --chunk_size 1024
+# ./datastreamclient --num_clients 1 --data_size 50 --num_iterations 10 --chunk_size 64
+# ./datastreamclient --num_clients 1 --data_size 50 --num_iterations 10 --chunk_size 128
+# ./datastreamclient --num_clients 1 --data_size 50 --num_iterations 10 --chunk_size 256
+# ./datastreamclient --num_clients 1 --data_size 50 --num_iterations 10 --chunk_size 512
+# ./datastreamclient --num_clients 1 --data_size 50 --num_iterations 10 --chunk_size 1024
+
+
+# ./datastreamclient --num_clients 1 --data_size 100 --num_iterations 10 --chunk_size 64
+# ./datastreamclient --num_clients 1 --data_size 100 --num_iterations 10 --chunk_size 128
+# ./datastreamclient --num_clients 1 --data_size 100 --num_iterations 10 --chunk_size 256
+# ./datastreamclient --num_clients 1 --data_size 100 --num_iterations 10 --chunk_size 512
+# ./datastreamclient --num_clients 1 --data_size 100 --num_iterations 10 --chunk_size 1024
+
+####### 2 clients
+# ./datastreamclient --num_clients 2 --data_size 10 --num_iterations 10 --chunk_size 64
+# ./datastreamclient --num_clients 2 --data_size 10 --num_iterations 10 --chunk_size 128
+# ./datastreamclient --num_clients 2 --data_size 10 --num_iterations 10 --chunk_size 256
+# ./datastreamclient --num_clients 2 --data_size 10 --num_iterations 10 --chunk_size 512
+# ./datastreamclient --num_clients 2 --data_size 10 --num_iterations 10 --chunk_size 1024
+
+# ./datastreamclient --num_clients 2 --data_size 50 --num_iterations 10 --chunk_size 64
+# ./datastreamclient --num_clients 2 --data_size 50 --num_iterations 10 --chunk_size 128
+# ./datastreamclient --num_clients 2 --data_size 50 --num_iterations 10 --chunk_size 256
+# ./datastreamclient --num_clients 2 --data_size 50 --num_iterations 10 --chunk_size 512
+# ./datastreamclient --num_clients 2 --data_size 50 --num_iterations 10 --chunk_size 1024
+
+
+# ./datastreamclient --num_clients 2 --data_size 100 --num_iterations 10 --chunk_size 64
+# ./datastreamclient --num_clients 2 --data_size 100 --num_iterations 10 --chunk_size 128
+# ./datastreamclient --num_clients 2 --data_size 100 --num_iterations 10 --chunk_size 256
+# ./datastreamclient --num_clients 2 --data_size 100 --num_iterations 10 --chunk_size 512
+# ./datastreamclient --num_clients 2 --data_size 100 --num_iterations 10 --chunk_size 1024
+
+
+####### 3 clients
+./datastreamclient --num_clients 3 --data_size 10 --num_iterations 10 --chunk_size 64
+./datastreamclient --num_clients 3 --data_size 10 --num_iterations 10 --chunk_size 128
+./datastreamclient --num_clients 3 --data_size 10 --num_iterations 10 --chunk_size 256
+./datastreamclient --num_clients 3 --data_size 10 --num_iterations 10 --chunk_size 512
+./datastreamclient --num_clients 3 --data_size 10 --num_iterations 10 --chunk_size 1024
+
+./datastreamclient --num_clients 3 --data_size 50 --num_iterations 10 --chunk_size 64
+./datastreamclient --num_clients 3 --data_size 50 --num_iterations 10 --chunk_size 128
+./datastreamclient --num_clients 3 --data_size 50 --num_iterations 10 --chunk_size 256
+./datastreamclient --num_clients 3 --data_size 50 --num_iterations 10 --chunk_size 512
+./datastreamclient --num_clients 3 --data_size 50 --num_iterations 10 --chunk_size 1024
+
+
+./datastreamclient --num_clients 3 --data_size 100 --num_iterations 10 --chunk_size 64
+./datastreamclient --num_clients 3 --data_size 100 --num_iterations 10 --chunk_size 128
+./datastreamclient --num_clients 3 --data_size 100 --num_iterations 10 --chunk_size 256
+./datastreamclient --num_clients 3 --data_size 100 --num_iterations 10 --chunk_size 512
+./datastreamclient --num_clients 3 --data_size 100 --num_iterations 10 --chunk_size 1024
+
+
 
 killall -9 datastreamserver || true
